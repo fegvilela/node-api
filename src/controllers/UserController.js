c9b33d4ece2504d93b3b696de625f1ecee64c0a6
Fernanda Vilela <fegvilela@gmail.com>
2020-09-19 18:05:54 -0300

    create soft delete for users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,7 @@ const knex = require("../database");
 
 module.exports = {
   async index(req, res) {
-    const results = await knex("users");
+    const results = await knex("users").where("deleted_at", null);
 
     return res.json(results);
   },
@@ -39,7 +39,7 @@ module.exports = {
     try {
       const { id } = req.params;
 
-      await knex("users").where({ id }).del();
+      await knex("users").where({ id }).update("deleted_at", new Date());
 
       return res.send();
     } catch (error) {
