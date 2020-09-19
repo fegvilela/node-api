df986f0691dde0c40c9402be408efc0069606a92
Fernanda Vilela <fegvilela@gmail.com>
2020-09-19 17:13:43 -0300

    add projects controller e get route

---
+++ src/controllers/ProjectsController.js
@@ -0,0 +1,14 @@
+/* eslint-disable consistent-return */
+const knex = require("../database");
+
+module.exports = {
+  async index(req, res, next) {
+    try {
+      const results = await knex("projects");
+
+      return res.json(results);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ const express = require("express");
 const routes = express.Router();
 
 const UserController = require("./controllers/UserController");
+const ProjectsController = require("./controllers/ProjectsController");
 
-routes.get("/users", UserController.index);
-routes.post("/users", UserController.create);
-routes.put("/users/:id", UserController.update);
-routes.delete("/users/:id", UserController.delete);
+routes
+  // users
+  .get("/users", UserController.index)
+  .post("/users", UserController.create)
+  .put("/users/:id", UserController.update)
+  .delete("/users/:id", UserController.delete)
+  // projects
+  .get("/projects", ProjectsController.index);
 
 module.exports = routes;
