fecdcdc258bb35957834b31fcc3981b3354904da
Fernanda Vilela <fegvilela@gmail.com>
2021-05-03 15:27:18 -0300

    fix: db prod connection

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,14 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_SSL,
+    },
     migrations: {
       tableName: "knex_migrations",
       directory: `${__dirname}/src/database/migrations`,
