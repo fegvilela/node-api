8ed4da82efe0bd574578049a1d7e1d832898e291
Fernanda Vilela <fegvilela@gmail.com>
2021-05-03 15:42:37 -0300

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
@@ -19,4 +21,4 @@ app.use((error, req, res, next) => {
   res.json({ error: error.message });
 });
 
-app.listen(3333, () => console.log("server is running"));
+app.listen(process.env.PORT || 3333, () => console.log("server is running"));
