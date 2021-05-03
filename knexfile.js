ac70cdfef65bc9ab7112617d2ddedb85bb9d3a02
Fernanda Vilela <fegvilela@gmail.com>
2021-05-03 15:19:34 -0300

    feat: add production knex connection

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,17 @@ module.exports = {
       directory: `${__dirname}/src/database/seeds`,
     },
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+  },
   onUpdateTrigger: (table) => `
   CREATE TRIGGER ${table}_updated_at
   BEFORE UPDATE ON ${table}
